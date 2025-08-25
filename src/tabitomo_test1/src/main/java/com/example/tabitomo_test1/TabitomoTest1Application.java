22c29438ebcfcb47491a219e31df1e56807ec477
MinHo Son <smh020525@gmail.com>
2025-08-25 14:29:18 +0900

    안녕하세요
    안녕하세요

---
+++ src/tabitomo_test1/src/main/java/com/example/tabitomo_test1/TabitomoTest1Application.java
@@ -0,0 +1,13 @@
+package com.example.tabitomo_test1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabitomoTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabitomoTest1Application.class, args);
+	}
+
+}
---
+++ src/tabitomo_test1/src/test/java/com/example/tabitomo_test1/TabitomoTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tabitomo_test1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabitomoTest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

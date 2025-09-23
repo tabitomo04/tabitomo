ca8d24397c388eb2825b42bc64f848c9a55c432a
MinHo Son <smh020525@gmail.com>
2025-09-23 17:17:24 +0900

    피드백 - 메일, 닉네임까지 저장완료

--- src/main/java/com/koreatravel/tabitomo/controller/chat/FeedbackController.java
+++ src/main/java/com/koreatravel/tabitomo/controller/chat/FeedbackController.java
@@ -1,13 +1,13 @@
 package com.koreatravel.tabitomo.controller.chat;
 
+import com.koreatravel.tabitomo.config.security.UserDetailsImpl;
 import com.koreatravel.tabitomo.domain.dto.chat.FeedbackRequest;
 import com.koreatravel.tabitomo.domain.entity.chat.Feedback;
 import com.koreatravel.tabitomo.service.chat.FeedbackService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
 
 @RestController
 @RequestMapping("/api")
@@ -21,15 +21,21 @@ public class FeedbackController {
     }
 
     @PostMapping("/feedback")
-    public ResponseEntity<?> receiveFeedback(@RequestBody FeedbackRequest feedbackRequest, @RequestParam(required = false) Long feedbackId) {
+    public ResponseEntity<?> receiveFeedback(@RequestBody FeedbackRequest feedbackRequest, @RequestParam(required = false) Long feedbackId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         try {
+            // 로그인한 사용자의 이메일과 닉네임을 FeedbackRequest에 추가
+            if (userDetails != null) {
+                // UserDetailsImpl 객체에서 직접 이메일과 닉네임을 가져와 설정합니다.
+                feedbackRequest.setEmail(userDetails.getEmail());
+                feedbackRequest.setNickname(userDetails.getNickname());
+            }
+
             // Service 메서드를 호출하여 피드백 저장 또는 업데이트
             Feedback savedFeedback = feedbackService.saveOrUpdateFeedback(feedbackRequest, feedbackId);
             // 저장된 피드백 객체를 반환하여 클라이언트가 ID를 관리할 수 있도록 함
             return ResponseEntity.ok().body(savedFeedback);
-        } catch (JsonProcessingException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("채팅 기록을 JSON으로 변환하는 데 실패했습니다.");
         } catch (Exception e) {
+            e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("피드백 저장/수정 중 오류가 발생했습니다.");
         }
     }
--- src/main/java/com/koreatravel/tabitomo/domain/dto/chat/FeedbackRequest.java
+++ src/main/java/com/koreatravel/tabitomo/domain/dto/chat/FeedbackRequest.java
@@ -4,15 +4,14 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Date;
-import java.util.List;
-import java.util.Map;
 
 @Getter
 @Setter
 public class FeedbackRequest {
+    private String email; // 추가
+    private String nickname; // 추가
     private Integer rating;
     private String feedbackText;
-    private List<Map<String, String>> chatHistory; // chatHistory 필드 추가
-    private Date timestamp;
     private Integer isHelpful;
+    private Date timestamp;
 }
